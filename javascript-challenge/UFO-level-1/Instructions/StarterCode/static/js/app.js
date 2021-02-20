3a53b5f9340ea887c942acc7da6a4431e524d85f
Jeremy Jackson <jcharles.jackson04@gmail.com>
2021-02-19 20:33:15 -0800

    Adding final files to the repository

--- javascript-challenge/UFO-level-1/Instructions/StarterCode/static/js/app.js
+++ javascript-challenge/UFO-level-1/Instructions/StarterCode/static/js/app.js
@@ -2,3 +2,19 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+let tbody = d3.select("tbody");
+
+function createTable(data) {
+
+    tbody.html("");
+
+    data.forEach((dataRow) => {
+        let tableRow = tbody.append("tr");
+
+        Object.values(dataRow).forEach((val) => {
+            let cell = tableRow.append('td');
+            cell.text(val);
+        });
+    });
+}
+createTable(tableData);
